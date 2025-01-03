a1609e13893d3de0d560365ad99bceaf7bb61191
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2025-01-03 17:14:10 -0300

    feat: schemas

--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -3,13 +3,34 @@ import { Schemas } from "../interfaces/abstract/schemas"
 import { SchemaType } from "../types/schema"
 
 export class ProductSchemas extends Schemas {
-    public response: SchemaType = z.object({})
+    public response: SchemaType = z.object({
+        tag: z.string(),
+        name: z.string(),
+        description: z.string().optional(),
+        fullDescription: z.string().optional(),
+        price: z.number(),
+        newPrice: z.number().optional(),
+    })
 
     public listResponse: SchemaType = z.array(this.response)
 
-    public create: SchemaType = z.object({})
+    public create: SchemaType = z.object({
+        tag: z.string(),
+        name: z.string(),
+        description: z.string().optional(),
+        fullDescription: z.string().optional(),
+        price: z.number().min(0),
+        newPrice: z.number().min(0).optional(),
+    })
 
-    public update: SchemaType = z.object({})
+    public update: SchemaType = z.object({
+        tag: z.string().optional(),
+        name: z.string().optional(),
+        description: z.string().optional(),
+        fullDescription: z.string().optional(),
+        price: z.number().min(0).optional(),
+        newPrice: z.number().min(0).optional(),
+    })
 
     public idParams: SchemaType = z.object({
         id: z.string().min(1, "Product ID is required"),
