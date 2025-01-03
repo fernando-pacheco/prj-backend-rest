d8872dee3cb1303defdbb7f3a7dab0319561f294
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2025-01-03 09:56:04 -0300

    feat: módulo de services

---
+++ src/services/product.ts
@@ -0,0 +1,52 @@
+import { PrismaClient, Product } from "@prisma/client"
+
+export class ProductServices {
+    private ProductModel: PrismaClient["product"]
+
+    constructor(private prisma: PrismaClient = new PrismaClient()) {
+        this.prisma = prisma
+        this.ProductModel = this.prisma.product
+    }
+
+    async createProduct(body: Product): Promise<Product> {
+        const product = await this.ProductModel.create({
+            data: {
+                name: body.name,
+                price: body.price,
+            },
+        })
+
+        return product
+    }
+
+    async getProducts(): Promise<Product[]> {
+        const products = await this.ProductModel.findMany()
+        return products
+    }
+
+    async getProductByID(id: string): Promise<Product | null> {
+        const product = await this.ProductModel.findUnique({
+            where: { id },
+        })
+
+        return product
+    }
+
+    async updateProduct(id: string, body: Product): Promise<Product> {
+        const product = await this.ProductModel.update({
+            where: { id },
+            data: {
+                name: body.name,
+                price: body.price,
+            },
+        })
+
+        return product
+    }
+
+    async deleteProductByID(id: string): Promise<void> {
+        await this.ProductModel.delete({
+            where: { id },
+        })
+    }
+}
