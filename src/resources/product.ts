53a756b46e5dd4ebb44b73ab5d65d3c050c1b955
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2025-01-03 09:56:38 -0300

    feat: módulo de resources

---
+++ src/resources/product.ts
@@ -0,0 +1,93 @@
+import { ProductServices } from "../services/product"
+import { ProductSchemas } from "../schemas/product"
+import { Resources } from "../interfaces/abstract/resources"
+import { Product } from "@prisma/client"
+import { FastifyReply } from "fastify"
+import { RequestData } from "../types/resource"
+
+export class ProductResources extends Resources<Product> {
+    constructor(
+        private service: ProductServices = new ProductServices(),
+        private schema: ProductSchemas = new ProductSchemas()
+    ) {
+        super()
+
+        this.create = this.create.bind(this)
+        this.list = this.list.bind(this)
+        this.get = this.get.bind(this)
+        this.update = this.update.bind(this)
+        this.delete = this.delete.bind(this)
+    }
+
+    public async create(
+        request: RequestData<{ name: string; id: string; price: number }>,
+        reply: FastifyReply
+    ): Promise<void> {
+        const body = request.body
+        const newProduct = await this.service.createProduct(body)
+        reply.status(201).send(newProduct)
+    }
+
+    public async list(): Promise<Product[]> {
+        const products = await this.service.getProducts()
+        return products
+    }
+
+    public async get(
+        request: RequestData<{ name: string; id: string; price: number }>,
+        reply: FastifyReply
+    ): Promise<void> {
+        const { id } = request.params
+        try {
+            const product = await this.ensureProductExists(id, reply)
+            reply.status(200).send(this.schema.response.parse(product))
+        } catch (error) {
+            this.handleError(reply, error, 400)
+        }
+    }
+
+    public async update(
+        request: RequestData<{ name: string; id: string; price: number }>,
+        reply: FastifyReply
+    ): Promise<void> {
+        const { id } = request.params
+        const body = request.body
+        try {
+            await this.ensureProductExists(id, reply)
+            if (Object.keys(body).length === 0) {
+                reply.status(400).send({ message: "Empty body." })
+                return
+            }
+
+            const updatedProduct = await this.service.updateProduct(id, body)
+            reply.status(200).send(updatedProduct)
+        } catch (error) {
+            this.handleError(reply, error, 400)
+        }
+    }
+
+    public async delete(
+        request: RequestData<{ name: string; id: string; price: number }>,
+        reply: FastifyReply
+    ): Promise<void> {
+        const { id } = request.params
+        try {
+            await this.ensureProductExists(id, reply)
+            await this.service.deleteProductByID(id)
+            reply
+                .status(200)
+                .send({ message: `Product {${id}} successfully deleted.` })
+        } catch (error) {
+            this.handleError(reply, error, 400)
+        }
+    }
+
+    private async ensureProductExists(id: string, reply: FastifyReply) {
+        const product = await this.service.getProductByID(id)
+        if (!product) {
+            reply.status(404).send({ message: "Product not found." })
+            return null
+        }
+        return product
+    }
+}
